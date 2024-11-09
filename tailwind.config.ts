204499289e365bf8afd7c18324fe791467f7ed08
JooKangSan <you04045@naver.com>
2024-11-09 21:23:31 +0900

    feat: image 추가 및 global.css 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,10 +9,25 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        "cool-gray50": "#f9fafb",
+        "cool-gray100": "#f3f4f6",
+        "cool-gray200": "#e5e7eb",
+        "cool-gray400": "#9ca3af",
+        "cool-gray500": "#6B7280",
+        "cool-gray600": "#4b5563",
+        "cool-gray700": "#374151",
+        "cool-gray800": "#1f2937",
+        "cool-gray900": "#111827",
+        "brand-blue": "#3692ff",
+        "sky-blue": "#cfe5ff",
       },
+    },
+    screens: {
+      sm: { min: "0px", max: "767px" },
+      md: { min: "768px", max: "1199px" },
+      lg: { min: "1200px" },
     },
   },
   plugins: [],
 } satisfies Config;
+
