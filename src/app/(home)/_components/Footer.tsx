d57b2025b503df1408ba5b9d89a557fe486aa3a3
JooKangSan <you04045@naver.com>
2024-11-10 20:51:15 +0900

    feat: footer 추가

--- src/app/(home)/_components/Footer.tsx
+++ src/app/(home)/_components/Footer.tsx
@@ -1,5 +1,83 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+function SocialLink({
+  href,
+  src,
+  alt,
+}: {
+  href: string;
+  src: string;
+  alt: string;
+}) {
+  return (
+    <Link
+      href={href}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="hover:opacity-80 transition-opacity"
+    >
+      <Image src={src} alt={alt} width={20} height={20} />
+    </Link>
+  );
+}
+
+const socialMediaLinks = [
+  {
+    href: 'https://facebook.com',
+    src: '/images/icons/ic_facebook.svg',
+    alt: '페이스북',
+  },
+  {
+    href: 'https://twitter.com',
+    src: '/images/icons/ic_twitter.svg',
+    alt: '트위터',
+  },
+  {
+    href: 'https://youtube.com',
+    src: '/images/icons/ic_youtube.svg',
+    alt: '유튜브',
+  },
+  {
+    href: 'https://instagram.com',
+    src: '/images/icons/ic_instagram.svg',
+    alt: '인스타그램',
+  },
+];
+
 function Footer() {
-  return <div></div>;
+  return (
+    <footer className="fixed bottom-0 flex flex-col w-full h-40 justify-between bg-[#111322] p-8">
+      <div className="w-full flex justify-between">
+        <p className="hidden tablet:block text-gray-500">@codeit - 2024</p>
+        <div className="flex justify-between items-center gap-8">
+          <Link
+            href="/privacy-policy"
+            className="text-[#676767] hover:text-gray-900"
+          >
+            Privacy Policy
+          </Link>
+          <Link
+            href="/faq"
+            className="text-gray-600 text-sm hover:text-gray-900"
+          >
+            FAQ
+          </Link>
+        </div>
+        <div className="flex gap-3">
+          {socialMediaLinks.map((link) => (
+            <SocialLink
+              key={link.alt}
+              href={link.href}
+              src={link.src}
+              alt={link.alt}
+            />
+          ))}
+        </div>
+      </div>
+      <p className="text-gray-500 tablet:hidden">@codeit - 2024</p>
+    </footer>
+  );
 }
 
 export default Footer;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,33 +1,31 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        "cool-gray50": "#f9fafb",
-        "cool-gray100": "#f3f4f6",
-        "cool-gray200": "#e5e7eb",
-        "cool-gray400": "#9ca3af",
-        "cool-gray500": "#6B7280",
-        "cool-gray600": "#4b5563",
-        "cool-gray700": "#374151",
-        "cool-gray800": "#1f2937",
-        "cool-gray900": "#111827",
-        "brand-blue": "#3692ff",
-        "sky-blue": "#cfe5ff",
+        'cool-gray50': '#f9fafb',
+        'cool-gray100': '#f3f4f6',
+        'cool-gray200': '#e5e7eb',
+        'cool-gray400': '#9ca3af',
+        'cool-gray500': '#6B7280',
+        'cool-gray600': '#4b5563',
+        'cool-gray700': '#374151',
+        'cool-gray800': '#1f2937',
+        'cool-gray900': '#111827',
+        'brand-blue': '#3692ff',
+        'sky-blue': '#cfe5ff',
       },
     },
     screens: {
-      sm: { min: "0px", max: "767px" },
-      md: { min: "768px", max: "1199px" },
-      lg: { min: "1200px" },
+      pc: '1280px',
+      tablet: '768px',
     },
   },
   plugins: [],
 } satisfies Config;
-
